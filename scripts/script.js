4392abbec8ba421ae225691e8d2edd4e17db47ed
Rafael Shepard <rafael_gulie94@list.ru>
2018-05-03 00:05:48 +0300

    f

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
   $('.circle').click(function(){
-    $('.header__container').animate({
-      bottom: '400px'
-    })
-    $('.circle__img').slideToggle('slow');
-    $('main').slideToggle('slow');
   });
 });
