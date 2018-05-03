6da2eccf98ee14d2e47e79c44c8c8c360626220c
Rafael Shepard <rafael_gulie94@list.ru>
2018-05-03 14:25:09 +0300

    title

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('.circle').click(function(){
-    $('main').slideToggle('2500');
+    $('main').slideToggle('2500'),
+    $('.main__title-box').toggleClass('main__title-box--active');
   });
 });
