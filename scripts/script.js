d857f7b6a4af6eea895f3c136ce6e97e683432e8
Rafael Shepard <rafael_gulie94@list.ru>
2018-05-04 14:37:32 +0300

    yo

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,41 @@
 $(document).ready(function(){
-  $('.circle').click(function(){
-    $('main').slideToggle('2500'),
-    $('.main__title-box').toggleClass('main__title-box--active');
+  var elem1 = $('#a');
+  var elem2 = $('#b');
+  var elem3 = $('#c');
+  var elem4 = $('.header__choice');
+
+  $('.a').on("click", function(e) {
+    e.preventDefault()
+    elem1.slideToggle().addClass('toggle');
+    elem2.hide();
+    elem3.hide();
+    elem4.toggleClass('new');
   });
+  $('.b').on("click", function(e) {
+    e.preventDefault();
+
+    elem1.hide();
+    elem2.slideToggle().addClass("toggle");
+    elem3.hide();
+    elem4.toggleClass("new");
+})
+
+$('.c').on("click", function(e) {
+
+    e.preventDefault();
+
+     elem1.hide();
+     elem2.hide();
+     elem3.slideToggle().addClass("toggle");
+     elem4.toggleClass("new");
+
+})
+
+$(document).on('click', '.toggle', function() {
+
+    elem1.hide();
+    elem2.hide();
+    elem3.hide();
+    elem4.removeClass('new');
+  })
 });
